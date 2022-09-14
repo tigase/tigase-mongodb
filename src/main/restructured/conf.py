f64f532a3f676e27add35b84155983b95ef6ffe1
Qian Luo <qianqianluo@gmail.com>
2022-09-14 16:25:52 -0700

    updates

--- src/main/restructured/conf.py
+++ src/main/restructured/conf.py
@@ -66,7 +66,7 @@ exclude_patterns = []
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 
-extensions = ['javasphinx','autoapi.extension']
+extensions = ['javasphinx','sphinx.ext.autodoc']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
